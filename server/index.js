278e72f6ff02c5147161770e3e26ea3095a543d5
mounika2121 <chmounika2121@gmail.com>
2024-02-04 20:10:32 +0530

    Getting each post id from db

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models');
+
+const postRouter = require('./routes/Posts');
+
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3007, () => {
+        console.log("Server is running on port 3007...");
+    });
+});
+

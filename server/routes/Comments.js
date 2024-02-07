7a17f45380096963629e7749979cc22dcb7d849d
mounika2121 <chmounika2121@gmail.com>
2024-02-07 20:03:50 +0530

    comment delete added

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -17,6 +17,28 @@ router.post('/', validationToken, async (req, res) => {
     comment.username = username;
     await Comments.create(comment);
     res.json(comment);
-})
+});
+
+router.delete('/:commentId', validationToken, async (req, res) => {
+    const commentId = req.params.commentId;
+
+    try {
+        const deletedCommentCount = await Comments.destroy({
+            where: {
+                id: commentId,
+            },
+        });
+
+        if (deletedCommentCount === 0) {
+            return res.status(404).json({ error: "Comment not found" });
+        }
+
+        res.json({ message: "Comment deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting comment:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
+
 
 module.exports = router;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -40,7 +40,7 @@ router.post("/login", async (req, res) => {
                 "importentsecret"
                 );
 
-            return res.json(accessToken);
+            return res.json({token: accessToken, username: username, id: user.id});
         });
     } catch (error) {
         console.error("Error:", error);
